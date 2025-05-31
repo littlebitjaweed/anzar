1ba142bdaa4099f98c82504129bd3c0ffc6ec72d
mjawadhussain40@gmail.com <mjawadhussain40@gmail.com>
2025-06-01 01:01:46 +0530

    new

--- pos_system/pos_system/settings.py
+++ pos_system/pos_system/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
